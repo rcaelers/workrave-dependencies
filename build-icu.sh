3fad2717790f2473e91eff52fe73c0e3cad26f50
Rob Caelers <rob.caelers@gmail.com>
2023-05-15 20:53:44 +0200

    Add icu4c

---
+++ build-icu.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash -xe
+
+PACKAGE=icu
+VERSION=73-1
+
+BASEDIR=$(dirname "$0")
+source ${BASEDIR}/config.sh
+
+export CMAKE_PREFIX_PATH=/opt/homebrew/opt/llvm/lib
+
+cd ${SOURCEDIR}
+git clone https://github.com/unicode-org/icu.git
+cd ${SOURCEDIR}/icu
+git checkout release-${VERSION}
+
+rm -rf ${BUILDDIR}
+mkdir -p ${BUILDDIR}
+
+cd ${BUILDDIR}
+CFLAGS="-arch x86_64 -arch arm64 -mmacosx-version-min=11.0" \
+CXXFLAGS="-std=c++20 -arch x86_64 -arch arm64 -mmacosx-version-min=11.0" \
+LDFLAGS="-headerpad_max_install_names -mmacosx-version-min=11.0" \
+    ${SOURCEDIR}/icu/icu4c/source/runConfigureICU MacOSX \
+        --prefix ${DEPLOYDIR} \
+        --disable-samples \
+        --disable-tests \
+        --enable-static \
+        --with-library-bits=64 \
+
+make
+make install
