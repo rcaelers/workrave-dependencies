8751ca82e902fc999b96d52f187d66161b4efe0c
Rob Caelers <rob.caelers@gmail.com>
2023-12-29 23:27:59 +0100

    Fix errors

--- build-boost.sh
+++ build-boost.sh
@@ -13,6 +13,5 @@ curl -OL https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/bo
 7z x ${SOURCEDIR}/boost_${VERSION_UNDERSCORES}.7z
 
 cd ${SOURCEDIR}/boost_${VERSION_UNDERSCORES}
-patch -p0 < ${ROOTDIR}/boost/arm+x86.patch
 ./bootstrap.sh --prefix=${DEPLOYDIR} --with-icu=${DEPLOYDIR}
 ./b2 --prefix=${DEPLOYDIR} --user-config=${ROOTDIR}/boost/user-config.jam -sNO_ZSTD=1 -sNO_LZMA=1 -sICU_PATH=${DEPLOYDIR} address-model=64 architecture=arm+x86 threading=multi link=shared abi=sysv binary-format=mach-o pch=off install
--- build-spdlog.sh
+++ build-spdlog.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -xe
 
 PACKAGE=spdlog
-VERSION=1.12.0
+VERSION=v1.12.0
 
 BASEDIR=$(dirname "$0")
 source ${BASEDIR}/config.sh
