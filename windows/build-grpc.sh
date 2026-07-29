4f8dfaeefe357388c213810278dee303b8e3ca8a
Rob Caelers <rob.caelers@gmail.com>
2026-07-29 20:00:07 +0200

    fix: skip symlink in grpc build

--- windows/build-grpc.sh
+++ windows/build-grpc.sh
@@ -7,7 +7,7 @@ package_dir="${script_dir}/mingw-w64-clang-x86_64-workrave-grpc"
 deploy_dir="${script_dir}/_deploy"
 
 package_path=$(cd "${package_dir}" && makepkg --packagelist)
-(cd "${package_dir}" && makepkg --noconfirm --force)
+(cd "${package_dir}" && makepkg --cleanbuild --noconfirm --force)
 
 mkdir -p "${deploy_dir}"
 install -Dm644 "${package_path}" "${deploy_dir}/workrave-grpc-mingw-clang64.pkg.tar.zst"
